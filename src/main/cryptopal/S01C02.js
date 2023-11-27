3f9cbf4746bf18ed927234d7718e6d6bd3d29a95
hungdoan-tech <hungdoan426@gmail.com>
2023-11-27 16:44:26 +0800

    Cryptopal S01C02 - Decoding and XOR two buffers

---
+++ src/main/cryptopal/S01C02.js
@@ -0,0 +1,34 @@
+// Fixed XOR
+// Write a function that takes two equal-length buffers and produces their XOR combination.
+
+// If your function works properly, then when you feed it the string:
+
+// 1c0111001f010100061a024b53535009181c
+// ... after hex decoding, and when XOR'd against:
+
+// 686974207468652062756c6c277320657965
+// ... should produce:
+
+// 746865206b696420646f6e277420706c6179
+
+function decodeAndXOR(firstBuffer, secondBuffer){
+
+    const firstCharArr = firstBuffer.split('')
+    const secondCharArr = secondBuffer.split('')
+
+    if( firstBuffer.length !== secondBuffer.length){
+        throw Error('Illegal Argument Exception - two input buffers are not equal')
+    }
+
+    finalResult = ''
+    for(let index = 0; index < firstBuffer.length; index++){
+        firstDec = parseInt(firstBuffer[index], 16)
+        secondDec = parseInt(secondBuffer[index], 16)
+        xorResult =  firstDec ^ secondDec
+        finalResult += xorResult
+    }
+
+    return finalResult
+}
+
+decodeAndXOR('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965')
