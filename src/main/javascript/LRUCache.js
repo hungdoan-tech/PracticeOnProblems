b99283cb6c732f6e7e5a13955e18e47e0390f030
hungdoan-tech <hungdoan426@gmail.com>
2023-12-15 23:32:06 +0800

    Enhance the TinyUTLib by using IFFE to achieve a better level of encapsulation and support more environments
    from now access the exposed object via _ut

--- src/main/javascript/LRUCache.js
+++ src/main/javascript/LRUCache.js
@@ -1,158 +1,168 @@
-import { assertEquals, tests } from "./TinyUTLib.js";
+import { _ut } from "./TinyUTLib.js";
+
+const { assertEquals, tests } = _ut;
 
 const oneDayInMiliseconds = 1000 * 60 * 60 * 24;
 
-export const LRUCache = function (capacity, defaultExpirationTimeInMiliseconds = oneDayInMiliseconds) {
+export const LRUCache = function (
+  capacity,
+  defaultExpirationTimeInMiliseconds = oneDayInMiliseconds
+) {
+  this.capacity = capacity;
 
-    this.capacity = capacity;
+  this.defaultExpirationTime = defaultExpirationTimeInMiliseconds;
 
-    this.defaultExpirationTime = defaultExpirationTimeInMiliseconds;
+  this.cache = new Map();
 
-    this.cache = new Map();
+  this.minTimeHeap = new MinTimingHeap();
 
-    this.minTimeHeap = new MinTimeHeap();
+  this.get = function (key) {
+    if (this.cache.has(key) === false) {
+      return -1;
+    }
 
-    this.get = function (key) {
-        if (this.cache.has(key) === false) {
-            return -1;
-        }
+    if (this.cache.get(key).until < performance.now()) {
+      this.cache.delete(key);
+      this.minTimeHeap.remove(key);
+      return -1;
+    }
 
-        if (this.cache.get(key).until < performance.now()) {
-            this.cache.delete(key);
-            this.minTimeHeap.remove(key);
-            return -1;
-        }
+    // best case - in cache and still not expired
+    this.minTimeHeap.remove(key);
+    this.minTimeHeap.insert(key);
+    const oldCache = this.cache.get(key);
 
-        // best case - in cache and still not expired
-        this.minTimeHeap.remove(key);
-        this.minTimeHeap.insert(key);
-        const oldCache = this.cache.get(key);
+    this.cache.set(key, {
+      value: oldCache.value,
+      expirationTime: oldCache.expirationTime,
+      until: performance.now() + oldCache.expirationTime,
+    });
+    return this.cache.get(key).value;
+  };
 
-        this.cache.set(key, {
-            value: oldCache.value,
-            expirationTime: oldCache.expirationTime,
-            until: performance.now() + oldCache.expirationTime
-        });
-        return this.cache.get(key).value;
-    };
+  this.put = function (
+    key,
+    value,
+    specificExpirationTimeInMiliseconds = this.defaultExpirationTime
+  ) {
+    if (this.cache.size >= this.capacity) {
+      const lruKey = this.minTimeHeap.extractMin();
+      this.cache.delete(lruKey);
+    }
 
-    this.put = function (key, value, specificExpirationTimeInMiliseconds = this.defaultExpirationTime) {
-        if (this.cache.size >= this.capacity) {
-            const lruKey = this.minTimeHeap.extractMin();
-            this.cache.delete(lruKey);
-        }
-
-        this.cache.set(key, {
-            value: value,
-            expirationTime: specificExpirationTimeInMiliseconds,
-            until: performance.now() + specificExpirationTimeInMiliseconds
-        });
-        this.minTimeHeap.insert(key);
-    };
+    this.cache.set(key, {
+      value: value,
+      expirationTime: specificExpirationTimeInMiliseconds,
+      until: performance.now() + specificExpirationTimeInMiliseconds,
+    });
+    this.minTimeHeap.insert(key);
+  };
 };
 
-const MinTimeHeap = function () {
+const MinTimingHeap = function () {
+  this.heap = [];
 
-    this.heap = [];
+  this.keyToIndex = new Map();
 
-    this.keyToIndex = new Map();
+  const Node = function (key) {
+    this.key = key;
+    this.time = performance.now();
+  };
 
-    const Node = function (key) {
-        this.key = key;
-        this.time = performance.now();
-    };
+  this.insert = function (key) {
+    const node = new Node(key);
+    this.heap.push(node);
+    this.keyToIndex.set(key, this.heap.length - 1);
+    this.heapifyUp();
+  };
 
+  this.remove = function (key) {
+    let index = this.keyToIndex.get(key);
+    if (index !== undefined) {
+      this.swap(index, this.heap.length - 1);
+      this.heap.pop();
+      this.keyToIndex.delete(key);
+      this.heapifyDown(index);
+    }
+  };
 
-    this.insert = function (key) {
-        const node = new Node(key);
-        this.heap.push(node);
-        this.keyToIndex.set(key, this.heap.length - 1);
-        this.heapifyUp();
-    };
+  this.extractMin = function () {
+    if (this.heap.length === 0) return null;
 
-    this.remove = function (key) {
-        let index = this.keyToIndex.get(key);
-        if (index !== undefined) {
-            this.swap(index, this.heap.length - 1);
-            this.heap.pop();
-            this.keyToIndex.delete(key);
-            this.heapifyDown(index);
-        }
-    };
+    const minValue = this.heap[0];
+    this.swap(0, this.heap.length - 1);
+    this.heap.pop();
+    this.keyToIndex.delete(minValue.key);
+    this.heapifyDown(0);
 
-    this.extractMin = function () {
-        if (this.heap.length === 0) return null;
+    return minValue.key;
+  };
 
-        const minValue = this.heap[0];
-        this.swap(0, this.heap.length - 1);
-        this.heap.pop();
-        this.keyToIndex.delete(minValue.key);
-        this.heapifyDown(0);
+  this.heapifyUp = function () {
+    let index = this.heap.length - 1;
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      if (this.heap[index].time < this.heap[parentIndex].time) {
+        this.swap(index, parentIndex);
+        index = parentIndex;
+      } else {
+        break;
+      }
+    }
+  };
 
-        return minValue.key;
-    };
+  this.heapifyDown = function (index) {
+    const leftChild = 2 * index + 1;
+    const rightChild = 2 * index + 2;
+    let smallest = index;
 
-    this.heapifyUp = function () {
-        let index = this.heap.length - 1;
-        while (index > 0) {
-            const parentIndex = Math.floor((index - 1) / 2);
-            if (this.heap[index].time < this.heap[parentIndex].time) {
-                this.swap(index, parentIndex);
-                index = parentIndex;
-            } else {
-                break;
-            }
-        }
-    };
+    if (
+      leftChild < this.heap.length &&
+      this.heap[leftChild].time < this.heap[smallest].time
+    ) {
+      smallest = leftChild;
+    }
 
-    this.heapifyDown = function (index) {
-        const leftChild = 2 * index + 1;
-        const rightChild = 2 * index + 2;
-        let smallest = index;
+    if (
+      rightChild < this.heap.length &&
+      this.heap[rightChild].time < this.heap[smallest].time
+    ) {
+      smallest = rightChild;
+    }
 
-        if (leftChild < this.heap.length &&
-            this.heap[leftChild].time < this.heap[smallest].time) {
-            smallest = leftChild;
-        }
+    if (smallest !== index) {
+      this.swap(index, smallest);
+      this.heapifyDown(smallest);
+    }
+  };
 
-        if (rightChild < this.heap.length &&
-            this.heap[rightChild].time < this.heap[smallest].time) {
-            smallest = rightChild;
-        }
+  this.swap = function (i, j) {
+    // reorg the key to index map
+    this.keyToIndex.set(this.heap[i].key, j);
+    this.keyToIndex.set(this.heap[j].key, i);
 
-        if (smallest !== index) {
-            this.swap(index, smallest);
-            this.heapifyDown(smallest);
-        }
-    };
-
-    this.swap = function (i, j) {
-        // reorg the key to index map
-        this.keyToIndex.set(this.heap[i].key, j);
-        this.keyToIndex.set(this.heap[j].key, i);
-
-        // swap
-        const tempValue = this.heap[i];
-        this.heap[i] = this.heap[j];
-        this.heap[j] = tempValue;
-    };
+    // swap
+    const tempValue = this.heap[i];
+    this.heap[i] = this.heap[j];
+    this.heap[j] = tempValue;
+  };
 };
-
 
 tests({
-    "give4EntriesKeyToValues_insertTheseEntriesToLRUTimmingCachesMax3Elements_expectedMissTheOldestElement": function () {
-        const lruCache = new LRUCache(3);
+  give4EntriesKeyToValues_insertTheseEntriesToLRUTimmingCachesMax3Elements_expectedMissTheOldestElement:
+    function () {
+      const lruCache = new LRUCache(3);
 
-        lruCache.put(10, 'one');
-        lruCache.put(2, 'two');
-        lruCache.put(30, 'three');
+      lruCache.put(10, "one");
+      lruCache.put(2, "two");
+      lruCache.put(30, "three");
 
-        assertEquals('one', lruCache.get(10));
+      assertEquals("one", lruCache.get(10));
 
-        lruCache.put(4, 'four', 0);
+      lruCache.put(4, "four", 0);
 
-        assertEquals(-1, lruCache.get(2));
-        assertEquals('three', lruCache.get(30));
-        assertEquals(-1, lruCache.get(4));
-    }
+      assertEquals(-1, lruCache.get(2));
+      assertEquals("three", lruCache.get(30));
+      assertEquals(-1, lruCache.get(4));
+    },
 });
--- src/main/javascript/TinyUTLib.js
+++ src/main/javascript/TinyUTLib.js
@@ -1,52 +1,79 @@
-const TinyUTLib = {
+export const _ut = (function (global, factoryFunc) {
+  // Check if the code is running in a Node.js environment
+  if (typeof exports === "object" && typeof module !== "undefined") {
+    // CommonJS: export the module
+    module.exports = factoryFunc();
+    return;
+  }
 
-    /**
-     * Pass your UT tests as field of an object, then each test will be run.
-     * See more about assert, assertEqual, eq, fail
-     * 
-     * @param {*} tests An object that contain multiple field, each field is a UT function
-     */
-    run: async function (tests) {
-        console.log('\n----------------------------------------------------------------------------------------------------------------\n');
-        let failures = 0;
-        for (let testName in tests) {
-            let testAction = tests[testName];
-            try {
-                if (Object.getPrototypeOf(testAction) === Object.getPrototypeOf(async function () { })) {
-                    await testAction();
-                } else {
-                    testAction();
-                }
-                console.log(`\nTEST: ${testName} PASSED`);
-                console.log('\n----------------------------------------------------------------------------------------------------------------\n');
-            } catch (e) {
-                failures++;
-                console.error(`\nTEST: ${testName} FAILED`);
-                console.error(e.stack);
-                console.log('\n----------------------------------------------------------------------------------------------------------------\n');
-            }
+  // ESM or other environment: attach the module to the global object
+  globalThis._ut = factoryFunc();
+
+  // Because in my pet project, I am currently setting my local NodeJs packages.json as  "type": "module"
+  // So at here I return it back the top level of the current module to export it for all other files can use and extract _ut
+  return global._ut;
+})(globalThis, function () {
+  /**
+   * Pass your UT tests as field of an object, then each test will be run.
+   * See more about assert, assertEqual, eq, fail
+   *
+   * @param {*} objectContainTestingFunction An object that contain multiple field, each field is a UT function
+   */
+  const tests = async function (objectContainTestingFunction) {
+    console.log(
+      "\n----------------------------------------------------------------------------------------------------------------\n"
+    );
+    let failures = 0;
+    for (let testName in objectContainTestingFunction) {
+      let testAction = objectContainTestingFunction[testName];
+      try {
+        if (
+          Object.getPrototypeOf(testAction) ===
+          Object.getPrototypeOf(async function () {})
+        ) {
+          await testAction();
+        } else {
+          testAction();
         }
-    },
+        console.log(`\nTEST: ${testName} PASSED`);
+        console.log(
+          "\n----------------------------------------------------------------------------------------------------------------\n"
+        );
+      } catch (e) {
+        failures++;
+        console.error(`\nTEST: ${testName} FAILED`);
+        console.error(e.stack);
+        console.log(
+          "\n----------------------------------------------------------------------------------------------------------------\n"
+        );
+      }
+    }
+  };
 
-    assertTrue: function (value, msg) {
-        if (value === True) {
-            throw new Error(`Assert: ${value} true but not get it: ${msg}`);
-        }
-    },
+  const assertTrue = function (value, msg) {
+    if (value === true) {
+      throw new Error(`Assert: ${value} true but not get it: ${msg}`);
+    }
+  };
 
-    assertFalse: function (value, msg) {
-        if (value !== True) {
-            throw new Error(`Assert: ${value} false but not get it: ${msg}`);
-        }
-    },
+  const assertFalse = function (value, msg) {
+    if (value === false) {
+      throw new Error(`Assert: ${value} false but not get it: ${msg}`);
+    }
+  };
 
-    assertEquals: function (expected, actual) {
-        if (expected !== actual) {
-            throw new Error(`Assert equals but the actual value ${actual} !== the expected value ${expected}`);
-        }
-    },
-};
+  const assertEquals = function (expected, actual) {
+    if (expected !== actual) {
+      throw new Error(
+        `Assert equals but the actual value ${actual} !== the expected value ${expected}`
+      );
+    }
+  };
 
-export const assertHasValue = TinyUTLib.assertHasValue;
-export const assertEquals = TinyUTLib.assertEquals;
-export const tests = TinyUTLib.run;
+  return {
+    tests: tests,
+    assertTrue: assertTrue,
+    assertFalse: assertFalse,
+    assertEquals: assertEquals,
+  };
+});
