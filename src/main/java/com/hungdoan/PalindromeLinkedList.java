3a07878ce24eae91eea8a84073252923ae705025
hungdoan-tech <hungdoan426@gmail.com>
2024-10-11 15:37:18 +0700

    Optimal solution for palindrome linked list:
    compare with the reverse halfback

--- src/main/java/com/hungdoan/PalindromeLinkedList.java
+++ src/main/java/com/hungdoan/PalindromeLinkedList.java
@@ -3,15 +3,20 @@ package com.hungdoan;
 import com.hungdoan.support.LinkedList;
 import com.hungdoan.support.ListNode;
 
+import java.util.Stack;
+
 public class PalindromeLinkedList {
 
     public static ListNode tempHead;
 
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
-        linkedList.addAll(new int[]{1, 2, 2, 1});
+        linkedList.addAll(new int[]{1, 2, 3, 4});
         tempHead = linkedList.head;
-        System.out.println(isPalindrome(linkedList.head));
+
+        PalindromeLinkedList palindromeLinkedList = new PalindromeLinkedList();
+        boolean palindrome = palindromeLinkedList.isPalindrome3(linkedList.head);
+        System.out.println(palindrome);
     }
 
     /**
@@ -22,7 +27,7 @@ public class PalindromeLinkedList {
      * @return
      */
     public static boolean isPalindrome(ListNode head) {
-        
+
         if (head == null) {
             return true;
         }
@@ -36,6 +41,97 @@ public class PalindromeLinkedList {
         }
 
         tempHead = tempHead.next;
+        return true;
+    }
+
+    public boolean isPalindrome2(ListNode head) {
+
+        if (head == null) {
+            return false;
+        }
+
+        if (head.next == null) {
+            return true;
+        }
+
+        if (head.next.next == null) {
+            return head.val == head.next.val;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode middle = slow;
+        if (fast.next == null) {
+            return handle(head, middle, middle.next);
+        }
+
+        if (middle.val != middle.next.val) {
+            return false;
+        }
+
+        return handle(head, middle, middle.next.next);
+    }
+
+    private boolean handle(ListNode head, ListNode middle, ListNode halfbackStarter) {
+        Stack<ListNode> halfback = new Stack<>();
+
+        ListNode runner = halfbackStarter;
+        while (runner != null) {
+            halfback.add(runner);
+            runner = runner.next;
+        }
+
+        runner = head;
+        while (runner != middle) {
+
+            if (runner.val != halfback.pop().val) {
+                return false;
+            }
+
+            runner = runner.next;
+        }
+
+        return true;
+    }
+
+    public boolean isPalindrome3(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode reversedHalfbackHead = null;
+
+        ListNode runner = slow.next;
+
+        while (runner != null) {
+
+            ListNode nxt = runner.next;
+            runner.next = reversedHalfbackHead;
+            reversedHalfbackHead = runner;
+            runner = nxt;
+        }
+
+        runner = head;
+        while (reversedHalfbackHead != null) {
+
+            if (reversedHalfbackHead.val != runner.val) {
+                return false;
+            }
+
+            reversedHalfbackHead = reversedHalfbackHead.next;
+            runner = runner.next;
+        }
+
         return true;
     }
 }
