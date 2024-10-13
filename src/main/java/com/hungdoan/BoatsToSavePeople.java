c7a68ee2aee5173ad15d9b3856d68d0ccd07a610
hungdoan-tech <hungdoan426@gmail.com>
2024-10-13 09:13:29 +0700

    Boat to save people via freq approach

--- src/main/java/com/hungdoan/BoatsToSavePeople.java
+++ src/main/java/com/hungdoan/BoatsToSavePeople.java
@@ -29,10 +29,43 @@ public class BoatsToSavePeople {
         return boatCount;
     }
 
+    public static int numRescueBoats2(int[] people, int limit) {
+        int[] freq = new int[limit + 1];
+        for (int weight : people) {
+            freq[weight]++;
+        }
+
+        int boats = 0;
+        int left = 0;
+        int right = limit;
+        while (left <= right) {
+
+            while (left <= right && freq[left] == 0) {
+                left++;
+            }
+
+            while (left <= right && freq[right] == 0) {
+                right--;
+            }
+
+            if (left > right) {
+                break;
+            }
+
+            boats++;
+            freq[right]--;
+            if (left + right <= limit) {
+                freq[left]--;
+            }
+        }
+
+        return boats;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 6, 7};
-        int limit = 8;
-        int i = numRescueBoats(arr, limit);
+        int[] arr = {2, 2};
+        int limit = 6;
+        int i = numRescueBoats2(arr, limit);
         System.out.println(i);
     }
 }
