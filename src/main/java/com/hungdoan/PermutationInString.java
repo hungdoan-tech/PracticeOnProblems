17d23c733c770af06266ac1fe174fbce0d87c9e9
hungdoan-tech <hungdoan426@gmail.com>
2024-10-31 15:03:17 +0700

    Cleaner version of the previous solution

--- src/main/java/com/hungdoan/PermutationInString.java
+++ src/main/java/com/hungdoan/PermutationInString.java
@@ -8,68 +8,60 @@ public class PermutationInString {
     public boolean checkInclusion(String s1, String s2) {
 
         Map<Character, Integer> charToFreq = new HashMap<>();
-        for (char currentChar : s1.toCharArray()) {
-            charToFreq.putIfAbsent(currentChar, 0);
-            charToFreq.put(currentChar, charToFreq.get(currentChar) + 1);
+        for (char c : s1.toCharArray()) {
+            charToFreq.put(c, charToFreq.getOrDefault(c, 0) + 1);
         }
-
+        Map<Character, Integer> originalCharToFreq = new HashMap<>(charToFreq);
 
         int left = 0;
-        boolean facingSeq = false;
+        boolean inSequence = false;
         for (int right = 0; right < s2.length(); right++) {
-
             char currentChar = s2.charAt(right);
+            Integer freq = charToFreq.get(currentChar);
 
-            if (charToFreq.containsKey(currentChar)) {
-                
-                facingSeq = true;
-
-                charToFreq.put(currentChar, charToFreq.get(currentChar) - 1);
-
-                if (isEmptyMap(charToFreq)) {
-                    return true;
-                }
-            }
-
-            if (!facingSeq) {
+            if (freq == null) {
+                inSequence = false;
                 left = right + 1;
-                charToFreq = new HashMap<>(charToFreq);
-                facingSeq = false;
+                charToFreq = new HashMap<>(originalCharToFreq);
                 continue;
             }
 
-            Character startChar = s2.charAt(left);
-            while (charToFreq.get(startChar) == 0) {
-                charToFreq.putIfAbsent(currentChar, 0);
-                charToFreq.put(currentChar, charToFreq.get(currentChar) + 1);
-                left++;
+            if (inSequence || freq > 0) {
+                inSequence = true;
+
+                if (freq == 0) {
+                    while (s2.charAt(left) != currentChar) {
+                        charToFreq.put(s2.charAt(left), charToFreq.get(s2.charAt(left)) + 1);
+                        left++;
+                    }
+                    left++;
+                    continue;
+                }
+
+                charToFreq.put(currentChar, freq - 1);
+                if (isMapEmpty(charToFreq)) {
+                    return true;
+                }
             }
         }
 
         return false;
     }
 
-    private boolean isEmptyMap(Map<Character, Integer> map) {
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            if (entry.getValue() != 0) {
+    private boolean isMapEmpty(Map<Character, Integer> map) {
+        for (int freq : map.values()) {
+            if (freq != 0) {
                 return false;
             }
         }
-
         return true;
     }
 
     public static void main(String[] args) {
         PermutationInString instance = new PermutationInString();
-        String s1 = "ab";
-        String s2 = "eidboaoo";
 
-        s1 = "ab";
-        s2 = "eidbaooo";
-
-        s1 = "adc";
-        s2 = "dcda";
-        boolean b = instance.checkInclusion(s1, s2);
-        System.out.println(b);
+        System.out.println(instance.checkInclusion("ab", "eidboaoo")); // should return false
+        System.out.println(instance.checkInclusion("ab", "eidbaooo")); // should return true
+        System.out.println(instance.checkInclusion("adc", "dcda"));    // should return true
     }
 }
