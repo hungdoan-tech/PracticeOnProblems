71787039f361143c8fbdf31893c1712e5ce14078
hungdoan-tech <hungdoan426@gmail.com>
2024-11-05 22:02:20 +0700

    Rewrite the Valid Palindrome

--- src/main/java/com/hungdoan/ValidPalindrome.java
+++ src/main/java/com/hungdoan/ValidPalindrome.java
@@ -1,51 +1,92 @@
 package com.hungdoan;
 
+//A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters,
+// it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+//
+//Given a string s, return true if it is a palindrome, or false otherwise.
+//
+//
+//
+//Example 1:
+//
+//Input: s = "A man, a plan, a canal: Panama"
+//Output: true
+//Explanation: "amanaplanacanalpanama" is a palindrome.
+//Example 2:
+//
+//Input: s = "race a car"
+//Output: false
+//Explanation: "raceacar" is not a palindrome.
+//Example 3:
+//
+//Input: s = " "
+//Output: true
+//Explanation: s is an empty string "" after removing non-alphanumeric characters.
+//Since an empty string reads the same forward and backward, it is a palindrome.
+//
+//
+//Constraints:
+//
+//1 <= s.length <= 2 * 105
+//s consists only of printable ASCII characters.
 public class ValidPalindrome {
 
     public static void main(String[] args) {
         String input = "A man, a plan, a canal: Panama";
-        System.out.println(isPalindrome(input));
+        input = "0P";
+        ValidPalindrome instance = new ValidPalindrome();
+        boolean palindrome = instance.isPalindrome(input);
+        System.out.println(palindrome);
     }
 
-    /**
-     * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same
-     * forward and backward. Alphanumeric characters include letters and numbers.
-     *
-     * Given a string s, return true if it is a palindrome, or false otherwise.
-     *
-     * @param s
-     * @return
-     */
-    public static boolean isPalindrome(String s) {
-        if (s.isEmpty()){
-            return false;
+    public boolean isPalindrome(String s) {
+        if (s.length() < 2) {
+            return true;
         }
 
-        char[] chars = s.toCharArray();
         int left = 0;
-        int right = chars.length - 1;
+        int right = s.length() - 1;
 
-        for(;left < right;){
+        while (left < right) {
 
-            if(Character.isLetterOrDigit(chars[left]) == false){
+            char leftChar = s.charAt(left);
+            if (!isAlphanumeric(leftChar)) {
                 left++;
                 continue;
             }
 
-            if(Character.isLetterOrDigit(chars[right]) == false){
+            char rightChar = s.charAt(right);
+            if (!isAlphanumeric(rightChar)) {
                 right--;
                 continue;
             }
 
-            if(Character.toLowerCase(chars[left]) != Character.toLowerCase(chars[right])){
+            if (!isEqualIgnoreCase(leftChar, rightChar)) {
                 return false;
             }
 
-            // equal -> match the left value and right value
             left++;
             right--;
         }
 
         return true;
     }
+
+    private boolean isAlphanumeric(char c) {
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+    }
+
+    private boolean isNumeric(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean isEqualIgnoreCase(char c1, char c2) {
+
+        if (isNumeric(c1) || isNumeric(c2)) {
+            return c1 == c2;
+        }
+
+        return c1 == c2 || c1 == c2 + 32 || c1 == c2 - 32;
+    }
 }
+
