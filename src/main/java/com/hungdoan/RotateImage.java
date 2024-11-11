8091fdc88fe19af375ea0fa3ec6e5183d1c445c5
hungdoan-tech <hungdoan426@gmail.com>
2024-11-11 22:24:36 +0700

    Update rotate image, now be able to input the angle as a multiple of 90

--- src/main/java/com/hungdoan/RotateImage.java
+++ src/main/java/com/hungdoan/RotateImage.java
@@ -27,6 +27,42 @@ public class RotateImage {
         }
     }
 
+    public void rotate(int[][] matrix, int angle) {
+        // Validate that the angle is a multiple of 90
+        if (angle % 90 != 0) {
+            throw new IllegalArgumentException("Angle must be a multiple of 90.");
+        }
+
+        // Normalize angle to be within [0, 360) by taking modulo 360
+        int effectiveAngle = ((angle % 360) + 360) % 360;
+
+        // Calculate the number of 90-degree clockwise rotations needed
+        int rotations = effectiveAngle / 90;
+
+        // Apply 90-degree clockwise rotations the required number of times
+        for (int i = 0; i < rotations; i++) {
+            rotate90Clockwise(matrix);
+        }
+    }
+
+    // Helper function to rotate the matrix by 90 degrees clockwise
+    private void rotate90Clockwise(int[][] matrix) {
+        int n = matrix.length - 1;
+
+        for (int layer = 0; layer <= n / 2; layer++) {
+            for (int pos = layer; pos < n - layer; pos++) {
+                int top = matrix[layer][pos];
+
+                // Rotate 4-way swap for 90-degree clockwise rotation
+                matrix[layer][pos] = matrix[n - pos][layer];
+                matrix[n - pos][layer] = matrix[n - layer][n - pos];
+                matrix[n - layer][n - pos] = matrix[pos][n - layer];
+                matrix[pos][n - layer] = top;
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         int[][] matrix = {
                 {1, 2, 3, 4},
@@ -37,7 +73,7 @@ public class RotateImage {
 
         RotateImage rotateImage = new RotateImage();
         rotateImage.rotate(matrix);
-        
+
         for (int[] row : matrix) {
             for (int num : row) {
                 System.out.print(num + " ");
